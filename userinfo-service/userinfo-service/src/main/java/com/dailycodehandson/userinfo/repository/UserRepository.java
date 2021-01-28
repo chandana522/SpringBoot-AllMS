a3ca87098b2b330f293ae2112d2ead555b9c3d15
[Chandana Deshpande] <chandana522@gmail.com>
2021-01-28 17:43:58 +0530

    first commit

---
+++ userinfo-service/userinfo-service/src/main/java/com/dailycodehandson/userinfo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dailycodehandson.userinfo.repository;
+
+import com.dailycodehandson.userinfo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUserId(Long userId);
+}
